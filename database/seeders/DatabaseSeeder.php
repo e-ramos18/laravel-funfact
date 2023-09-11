08a66affc5ce1a558b5be8d46fcbf055a20bb7c2
Eric Ramos <ericramosofficial@gmail.com>
2023-09-11 10:12:05 +0800

    Setup database and seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Database\Seeders\AdminUserSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(AdminUserSeeder::class);
+        // Call other seeders if needed
+    }
+}
